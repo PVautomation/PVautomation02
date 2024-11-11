952b1c6d083399ca538d41c9f4459419f87ace55
pvenkatarajan <pvenkatarajan@HL6T2B3.republic.com>
2024-11-10 19:05:41 -0600

    commit on nov 10, 2024

---
+++ JavaMavenTestNG/src/test/java/JavaMavenTestNG/JavaMavenTestNG/NewTest.java
@@ -0,0 +1,45 @@
+package JavaMavenTestNG.JavaMavenTestNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+  @Test
+  public void test1() {
+	  System.out.println("test1");
+  }
+  
+  @Test
+  public void test2() {
+	  System.out.println("test2");
+  }
+  
+  @Test
+  public void test3() {
+	  System.out.println("test3");
+  }
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("beforeMethod");
+  }
+  
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("afterMethod");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("before class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("after class");
+  }
+
+}
